3a9c9fe00bf33a4f7475631319617b757897b844
Ajay Sinh Jadeja <ajaysinhbjadeja659@gmail.com>
2025-07-13 15:26:33 +0530

    Secure database connection using .env

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL = "mysql+pymysql://root:123456789@localhost:3306/trackband"
+from dotenv import load_dotenv
+import os
+load_dotenv()
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+#DATABASE_URL = "mysql+pymysql://root:123456789@localhost:3306/trackband"
 engine = create_engine(DATABASE_URL, echo=True)
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, bind=engine)
